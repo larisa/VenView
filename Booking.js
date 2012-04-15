13e4c85116d039cae512be8363e78c76f8140553
larisa <larisa.berger@gmail.com>
2012-04-15 19:31:16 -0400

    created buttons, right now they're all layered on top of each other.

--- Booking.js
+++ Booking.js
@@ -1,15 +1,21 @@
 //for now represent pay Amt as string of $ ($, $$ or $$$)
 //start time is int on the hour
 //dates are 1-# of tour dates for now (where 1 is first day of tour)
+
 var Booking = function(venueName, date, startTime, duration, payAmt){
 		this.venue = venueName;
+		this.date = date;
 		this.startTime = startTime;
 		this.duration = duration;
-//		this.dollars = payAmt;
-		this.dollars = "$$$";
+		this.dollars = payAmt;
 		this.color = assignColor(venueName);
+
+		this.toString = function(){
+				var name = this.venue + this.date;
+				return name;
+		}
 }
 
 function assignColor(venueName){
-		return #30B0DV;
+		return "#30B0DV";
 }
--- schedule.js
+++ schedule.js
@@ -30,37 +30,66 @@ $(document).ready(function() {
 
 
 function make_money_btns(){
-		console.log("in make money btns");
 		var venueOpenings = [9];
 		var venueNames = ["Paradise", "Beehive", "House of Blues",
 											"Paradise", "Beehive", "House of Blues",
 										 "Paradise", "Beehive", "House of Blues"];
 		var allDates = [1, 4, 3, 2, 3, 2, 4, 5, 5];
 		var startTimes = [10, 15, 18, 19, 20, 10, 23, 22, 4]; 
+		var allDur = [40, 40, 40, 40, 40, 40, 40, 40, 40];
+		var allDollars = ["$$$", "$$", "$$", "$", "$$", "$", "$$$", "$$", "$"];
+
 		for (var i=0; i<=9; i++){
 				var gig = new Booking(venueNames[i], allDates[i], startTimes[i], 
 															allDur[i], allDollars[i]);
 				venueOpenings.push(gig);
 		}
 		for (var i=0; i<venueOpenings.length; i++){
+				console.log("making button allegedly");
 				var bookBtnInfo = venueOpenings[i];
-				var bookBtn = new span();
+				var bookBtn = document.createElement("button");
 				bookBtn.id = "moneyBtn" + i;
 				bookBtn.className = "moneyBtn";
 				bookBtn.type = "button";
 				bookBtn.name = bookBtnInfo.dollars;
+				bookBtn.value = bookBtnInfo.dollars;
+
+				$("moneyButton" + i, "moneyBtn").click(function() { return false;});
+
 
 				//TODO figure out position based on venue name + date
 				bookBtn.style.position = "absolute";
-				//bookBtn.style.height = ;
-//				bookBtn.style.width = 75%;
+				
+				bookBtn.style.height = 20px;
+				bookBtn.style.width = 100px;
 				//bookBtn.style.top = + "px";
 				//bookBtn.style.left = + "px";
-				bookBtn.style.background-color = bookBtnInfo.color;
+//				bookBtn.style.background-color = bookBtnInfo.color;
 						
-				allMoneyBtns.appendChild(nextBtn);
+				allMoneyBtns.appendChild(bookBtn);
 
 				}
 
 }
 		
+//for now represent pay Amt as string of $ ($, $$ or $$$)
+//start time is int on the hour
+//dates are 1-# of tour dates for now (where 1 is first day of tour)
+
+var Booking = function(venueName, date, startTime, duration, payAmt){
+		this.venue = venueName;
+		this.date = date;
+		this.startTime = startTime;
+		this.duration = duration;
+		this.dollars = payAmt;
+		this.color = assignColor(venueName);
+
+		this.toString = function(){
+				var name = this.venue + this.date;
+				return name;
+		}
+}
+
+function assignColor(venueName){
+		return new color("#30B0DV");
+}
