ab2d583523c50e5db6b99e21553382bb3c776a99
larisa <larisa.berger@gmail.com>
2012-04-15 13:48:41 -0400

    abstracted hiba's javascript code to the schedule.js file

--- schedule.js
+++ schedule.js
@@ -1,2 +1,15 @@
-//allow Javascript code inside this block to only run when the page has finished loading.
-
+function addDays(){
+	for(var count = 1; var<32; count++){
+		var innerText = document.createTextNode(count);
+		var day = document.createElement("option");
+		day.appendChild(innerText);
+		document.getElementById("days").appendChild(day);	
+	}
+}
+$(document).ready(function() {
+	addDays();
+	
+});
+$("#Enter").click(function(evt) {
+	document.getElementById('#Column2').style.visibility="visible";
+});
